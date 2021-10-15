a0373f44ba13983e4b03271ccedb9598bf28968e
Rittick Datta <drittick@gmail.com>
2021-10-15 12:05:34 +0530

    Added money color; Code Formatting;

--- chickencounter/src/components/Navbar/Navbar.tsx
+++ chickencounter/src/components/Navbar/Navbar.tsx
@@ -1,74 +1,59 @@
 import styles from './Navbar.module.css';
-import React from 'react'
+import React from 'react';
 import Button from '../../common/UI/Button/Button';
-import logo from "../../assets/img/logo.png";
+import logo from '../../assets/img/logo.png';
 
-function Navbar(props:any) {
-    const {
-        priceUpdateHandler,
-        workRecordHandler
-    } = props;
-    return (
-        <nav>
-            <ul className={styles.navbar}>
-                <li>
-                    <div>
-                        <img src={logo} alt="Mangso" width="100" />
-                        <h1>"Hi Bolin! {`You have no mangso today.`}"</h1>
-                    </div>
-                </li>
-                <li className={styles.infoBarWrapper}>
-                    <div className={styles.infoBar}>
-                        <div className={styles.infobar_infoblock}>
-                            <div className={styles.infobar_infoTitle}>
-                                Today
-                            </div>
-                            <div className={styles.infobar_infoVal}>
-                                {0}
-                            </div>
-                        </div>
-                        <div className={styles.infobar_infoblock}>
-                            <div className={styles.infobar_infoTitle}>
-                                Week
-                            </div>
-                            <div className={styles.infobar_infoVal}>
-                                {0}
-                            </div>
-                        </div>
-                        <div className={styles.infobar_infoblock}>
-                            <div className={styles.infobar_infoTitle}>
-                                Month
-                            </div>
-                            <div className={styles.infobar_infoVal}>
-                                {0}
-                            </div>
-                        </div>
-                        <div className={styles.infobar_infoblock}>
-                            <div className={styles.infobar_infoTitle}>
-                                Year
-                            </div>
-                            <div className={styles.infobar_infoVal}>
-                                {0}
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li className={styles.actionBarWrapper}>
-                    <div>
-                        <ul className={styles.actionBar}>
-                            <li>
-                                <Button text='Update Price' clickHandler={priceUpdateHandler}/>
-                            </li>
-                            <li>
-                                <Button text='Work Record' clickHandler={workRecordHandler}/>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+function Navbar(props: any) {
+  const { priceUpdateHandler, workRecordHandler } = props;
+  return (
+    <nav>
+      <ul className={styles.navbar}>
+        <li>
+          <div>
+            <img src={logo} alt='Mangso' width='100' />
+            <h1>"Hi Bolin! {`You have no mangso today.`}"</h1>
+          </div>
+        </li>
+        <li className={styles.infoBarWrapper}>
+          <div className={styles.infoBar}>
+            <div className={styles.infobar_infoblock}>
+              <div className={styles.infobar_infoTitle}>Today</div>
+              <div className={styles.infobar_infoVal}>{0}</div>
+              <div className={styles.infobar_money}>&#x20B9;{0}</div>
+            </div>
+            <div className={styles.infobar_infoblock}>
+              <div className={styles.infobar_infoTitle}>Week</div>
+              <div className={styles.infobar_infoVal}>{0}</div>
+              <div className={styles.infobar_money}>&#x20B9;{0}</div>
+            </div>
+            <div className={styles.infobar_infoblock}>
+              <div className={styles.infobar_infoTitle}>Month</div>
+              <div className={styles.infobar_infoVal}>{0}</div>
+              <div className={styles.infobar_money}>&#x20B9;{0}</div>
+            </div>
+            <div className={styles.infobar_infoblock}>
+              <div className={styles.infobar_infoTitle}>Year</div>
+              <div className={styles.infobar_infoVal}>{0}</div>
+              <div className={styles.infobar_money}>&#x20B9;{0}</div>
+            </div>
+          </div>
+        </li>
+        <li className={styles.actionBarWrapper}>
+          <div>
+            <ul className={styles.actionBar}>
+              <li>
+                <Button text='Update Price' clickHandler={priceUpdateHandler} />
+              </li>
+              <li>
+                <Button text='Work Record' clickHandler={workRecordHandler} />
+              </li>
             </ul>
-            {/* <Button /> */}
-        </nav>
-    )
+          </div>
+        </li>
+      </ul>
+      {/* <Button /> */}
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
